6d52b09098677c65233dd3707cc7480496adc109
ahmedsa8813-coder <ahmed.sa8813@gmail.com>
2026-08-04 19:25:35 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ForemanSystem, ForemanManagePage } from './App6';
+
+function Root() {
+  const params = new URLSearchParams(window.location.search);
+  if (params.get("foreman") === "1") return <ForemanSystem />;
+  if (params.get("manager") === "1") return <ForemanManagePage />;
+  return <App />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Root />);
